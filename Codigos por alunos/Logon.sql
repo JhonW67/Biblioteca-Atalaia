a94cfc0600aafb0f7c7d69e3d558b9ed71fcb124
FelipeBoabaid <felipeboabaid2012@hotmail.com>
2024-11-25 10:07:45 -0400

    Update Logon.sql

--- Codigos por alunos/Logon.sql
+++ Codigos por alunos/Logon.sql
@@ -1,12 +1,12 @@
 -- Criação de usuários e permissões de acesso
 CREATE USER 'bibliotecario'@'localhost' IDENTIFIED BY 'senha123';
-GRANT SELECT, INSERT, UPDATE, DELETE ON BibliotecaAtalaia.* TO 'bibliotecario'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON biblioteca_atalaia.* TO 'bibliotecario'@'localhost';
 
 CREATE USER 'leitor'@'localhost' IDENTIFIED BY 'senha123';
-GRANT SELECT ON BibliotecaAtalaia.Livro TO 'leitor'@'localhost';
+GRANT SELECT ON biblioteca_atalaia.livro TO 'leitor'@'localhost';
 
 CREATE USER 'administrador'@'localhost' IDENTIFIED BY 'admin123';
-GRANT ALL PRIVILEGES ON BibliotecaAtalaia.* TO 'administrador'@'localhost';
+GRANT ALL PRIVILEGES ON biblioteca_atalaia.* TO 'administrador'@'localhost';
 
 -- Finalização e commit
 FLUSH PRIVILEGES;
